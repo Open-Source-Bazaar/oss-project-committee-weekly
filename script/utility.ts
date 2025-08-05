14e190eeebc43ae29107ffde08aa0b3e55491968
TechQuery <shiy2008@gmail.com>
2025-08-05 10:21:33 +0800

    [optimize] update MobX-Lark

--- script/utility.ts
+++ script/utility.ts
@@ -3,7 +3,7 @@
 import { env } from "node:process";
 // @deno-types="npm:@types/fs-extra"
 import { outputFile } from "npm:fs-extra";
-import MobxLark, { LarkPageData, Wiki } from "npm:mobx-lark@2.4.0-rc.9";
+import MobxLark, { LarkPageData, Wiki } from "npm:mobx-lark@2.4.0";
 // @deno-types="npm:@types/react-dom/server"
 import { renderToStaticMarkup } from "npm:react-dom/server";
 
@@ -40,9 +40,10 @@ interface LarkWikiTraverseNode {
   markdown: string;
 }
 
-const documentStore = new MyDocumentModel();
-
-export async function docx2markdown(obj_token: string) {
+export async function docx2markdown(
+  documentStore: MyDocumentModel,
+  obj_token: string
+) {
   const blocks = await documentStore.getOneBlocks(
     obj_token,
     (token) => `https://kaiyuanshe.cn/api/lark/file/${token}`
@@ -66,13 +67,14 @@ export async function* traverseWiki(
   domain: string,
   space_id: string
 ): AsyncGenerator<LarkWikiTraverseNode> {
-  const wikiNodeStore = new MyWikiNodeModel(domain, space_id);
+  const wikiNodeStore = new MyWikiNodeModel(domain, space_id),
+    documentStore = new MyDocumentModel(domain);
 
   const nodeStream = wikiNodeStore.traverseTree();
 
   for await (const { obj_type, obj_token, title_path } of nodeStream)
     if (obj_type === "docx") {
-      const markdown = await docx2markdown(obj_token);
+      const markdown = await docx2markdown(documentStore, obj_token);
 
       yield { titlePath: title_path!, markdown };
     }
